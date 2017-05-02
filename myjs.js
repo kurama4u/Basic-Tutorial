235250c0486de0b8fe0ca0ed55a809218be4a3bf
kuramalicious <jv_kurama@yahoo.com>
2017-05-02 22:49:10 +0800

    my javascript

---
+++ myjs.js
@@ -0,0 +1,13 @@
+
+
+/*function ifunction (qty,para) {
+    var way = qty + para;
+    alert (way);
+}
+
+ifunction (500," ways to die");*/
+var name = "charles";
+ if ( name == "charles" ){
+alert ("this is not human");
+ }
+ else {alert ("im not a robot");}
---
+++ myjs2.js
@@ -0,0 +1,12 @@
+var qty1 = 1;
+var qty2 = 2;
+
+qty1 = qty1 + qty2;
+qty1++;
+//document.write(qty1);
+document.getElementById("demo").innerHTML=qty1;
+
+
+qty1--;
+
+document.getElementById("demo1").innerHTML=qty1;
