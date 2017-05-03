618894dae03fe471e70379963a4bc07114d7d1a8
kuramalicious <jv_kurama@yahoo.com>
2017-05-03 21:10:25 +0800

    conflict function

--- myjs.js
+++ myjs.js
@@ -1,13 +1,13 @@
-
-
-/*function ifunction (qty,para) {
-    var way = qty + para;
-    alert (way);
-}
-
-ifunction (500," ways to die");*/
 var name = "charles";
  if ( name == "charles" ){
 alert ("this is not human");
  }
  else {alert ("im not a robot");}
+
+function clickme() {
+var string1 = "GG ";
+var string2 = "W ";
+var string3 = "P";
+var string4 = string1.concat(string2, string3);
+document.getElementById("demo2").innerHTML = string4;
+}
--- myjs2.js
+++ myjs2.js
@@ -10,3 +10,11 @@ document.getElementById("demo").innerHTML=qty1;
 qty1--;
 
 document.getElementById("demo1").innerHTML=qty1;
+
+function clickme() {
+var string1 = "GG ";
+var string2 = "W ";
+var string3 = "P";
+var string4 = string1.concat(string2, string3);
+document.getElementById("demo2").innerHTML = string4;
+}
