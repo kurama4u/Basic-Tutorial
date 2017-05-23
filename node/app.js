083e59c0789c9e755ef34cfc15344140617aaed0
kuramalicious <jv_kurama@yahoo.com>
2017-05-23 21:36:30 +0800

    bundle

--- node/app.js
+++ node/app.js
@@ -1,9 +1,19 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type":"text/plain"});
-    res.end("My first Node.js server...");
+if(fs.readFile('index.html', (err, data) => {
+    if(err) {
+        throw err;
+    }
+
+    var server = http.createServer(function(req, res){
+    res.writeHead(200, {"Content-Type":"text/html"});
+    res.write(data);
+    res.end();
 });
 server.listen(8080, function(){
     console.log("server started");
+ 
 });
+}));
+
